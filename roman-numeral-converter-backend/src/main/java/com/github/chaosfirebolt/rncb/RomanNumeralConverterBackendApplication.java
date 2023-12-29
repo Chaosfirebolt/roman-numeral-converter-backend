57fcc524eb059e24c67c92a93dd154b97dee70e4
Boyan Georgiev <boyan_georgiev88@abv.bg>
2023-12-29 18:07:13 +0200

    initial commit

---
+++ roman-numeral-converter-backend/src/main/java/com/github/chaosfirebolt/rncb/RomanNumeralConverterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.chaosfirebolt.rncb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanNumeralConverterBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RomanNumeralConverterBackendApplication.class, args);
+  }
+
+}
