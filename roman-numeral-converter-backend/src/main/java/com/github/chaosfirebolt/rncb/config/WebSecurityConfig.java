5babf401f583f26245052e4f9be6f6a3f8ffb8f8
Boyan Georgiev <boyan_georgiev88@abv.bg>
2023-12-30 18:14:12 +0200

    add basic authentication

--- roman-numeral-converter-backend/src/main/java/com/github/chaosfirebolt/rncb/config/WebSecurityConfig.java
+++ roman-numeral-converter-backend/src/main/java/com/github/chaosfirebolt/rncb/config/WebSecurityConfig.java
@@ -3,8 +3,10 @@ package com.github.chaosfirebolt.rncb.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +16,11 @@ public class WebSecurityConfig {
 	@Bean
 	@Autowired
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll());
+		http.authorizeHttpRequests(requests -> requests
+						.requestMatchers("/convert").authenticated()
+						.requestMatchers("/app/register").permitAll())
+						.httpBasic(Customizer.withDefaults())
+						.csrf(AbstractHttpConfigurer::disable);
 		return http.build();
 	}
 }
