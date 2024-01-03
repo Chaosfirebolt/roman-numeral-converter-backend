07f7f0878498a36c377a947d30d690bd953cbd91
Boyan Georgiev <boyan_georgiev88@abv.bg>
2024-01-03 10:05:25 +0200

    cors config
    -  allow all

--- roman-numeral-converter-backend/src/main/java/com/github/chaosfirebolt/rncb/config/WebSecurityConfig.java
+++ roman-numeral-converter-backend/src/main/java/com/github/chaosfirebolt/rncb/config/WebSecurityConfig.java
@@ -8,6 +8,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -15,12 +20,24 @@ public class WebSecurityConfig {
 
 	@Bean
 	@Autowired
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource customCorsConfig) throws Exception {
 		http.authorizeHttpRequests(requests -> requests
 						.requestMatchers("/convert").authenticated()
 						.requestMatchers("/app/register").permitAll())
 						.httpBasic(Customizer.withDefaults())
-						.csrf(AbstractHttpConfigurer::disable);
+						.csrf(AbstractHttpConfigurer::disable)
+						.cors(config -> config.configurationSource(customCorsConfig));
 		return http.build();
+	}
+
+	@Bean("customCorsConfig")
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("*"));
+		configuration.setAllowedMethods(List.of("*"));
+		configuration.setAllowedHeaders(List.of("*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
